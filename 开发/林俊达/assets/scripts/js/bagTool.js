5678e9aed3a414d9a3d4f49c7b0254498190138d
huikserf <971555890@qq.com>
2022-11-25 11:02:47 +0800

    第三次提交：实现动画播放

--- 开发/林俊达/assets/scripts/js/bagTool.js
+++ 开发/林俊达/assets/scripts/js/bagTool.js
@@ -1,34 +1,34 @@
-cc.Class({
-    extends: cc.Component,
+// cc.Class({
+//     extends: cc.Component,
 
-    properties: {
-    },
+//     properties: {
+//     },
 
-    // LIFE-CYCLE CALLBACKS:
+//     // LIFE-CYCLE CALLBACKS:
 
-    initInfo (info) {
-        // 初始化该道具相关信息
+//     initInfo (info) {
+//         // 初始化该道具相关信息
 
-        // 图片
-        var self = this;
-        cc.loader.loadRes(info['picUrl'], cc.SpriteFrame, function (err, spriteFrame) {
-            self.node.getComponent(cc.Sprite).spriteFrame = spriteFrame;
-        });
+//         // 图片
+//         var self = this;
+//         cc.loader.loadRes(info['picUrl'], cc.SpriteFrame, function (err, spriteFrame) {
+//             self.node.getComponent(cc.Sprite).spriteFrame = spriteFrame;
+//         });
 
-        // 介绍
-        this.node.intro = info['intro'];
-        this.node.on(cc.Node.EventType.MOUSE_MOVE, function () {
-            let searchNode = cc.find("Canvas/intro"); //通过全路径获取对应节点
-            searchNode.getComponent(cc.Label).string=this.node.intro;
-            }.bind(this), this);
+//         // 介绍
+//         this.node.intro = info['intro'];
+//         this.node.on(cc.Node.EventType.MOUSE_MOVE, function () {
+//             let searchNode = cc.find("Canvas/intro"); //通过全路径获取对应节点
+//             searchNode.getComponent(cc.Label).string=this.node.intro;
+//             }.bind(this), this);
 
-        //点击使用物品会使物品图片出现在左上角
-        this.node.pic=info['picUrl'];
-        this.node.on(cc.Node.EventType.MOUSE_DOWN, function () {
-            let usetool = cc.find("Canvas/usetool"); //通过全路径获取对应节点
-            cc.loader.loadRes(info['picUrl'], cc.SpriteFrame, function (err, spriteFrame) {
-                usetool.getComponent(cc.Sprite).spriteFrame = spriteFrame;
-            });
-            }.bind(this), this);
-    },
-});
+//         //点击使用物品会使物品图片出现在左上角
+//         this.node.pic=info['picUrl'];
+//         this.node.on(cc.Node.EventType.MOUSE_DOWN, function () {
+//             let usetool = cc.find("Canvas/usetool"); //通过全路径获取对应节点
+//             cc.loader.loadRes(info['picUrl'], cc.SpriteFrame, function (err, spriteFrame) {
+//                 usetool.getComponent(cc.Sprite).spriteFrame = spriteFrame;
+//             });
+//             }.bind(this), this);
+//     },
+// });
